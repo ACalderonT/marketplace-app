1415d6b38bd2aff398c9f640cb51c2af5ab2d4b4
Alex Calderón Toledo <ac.calderont@gmail.com>
2024-08-04 15:32:04 -0400

    WIP 2

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -42,53 +42,55 @@ const Login = () => {
 
     return(
         <>
-            <Row className="login-form" align='middle' justify='center'>
-                <Col>
-                    <Form
-                        layout="vertical"
-                        name="login"
-                        onFinish={onFinish}
-                    >
-                        <h1>Welcome to MarketPlace</h1>
+            <div className='main-section'>
+                <Row className="login-form" align='middle' justify='center'>
+                    <Col>
+                        <Form
+                            layout="vertical"
+                            name="login"
+                            onFinish={onFinish}
+                        >
+                            <h1>Welcome to MarketPlace</h1>
 
-                        <Form.Item 
-                            label="Email"
-                            name="email"
-                            rules={[
-                                {
-                                    type: "email",
-                                    message: "The input is not a valid email!"
-                                },
-                                {
+                            <Form.Item 
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    {
+                                        type: "email",
+                                        message: "The input is not a valid email!"
+                                    },
+                                    {
+                                        required: true,
+                                        message: 'Please input your email!.'
+                                    }
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[{
                                     required: true,
-                                    message: 'Please input your email!.'
-                                }
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
+                                    message: "Please input your password!."
+                                }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
 
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[{
-                                required: true,
-                                message: "Please input your password!."
-                            }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
-
-                        <Form.Item>
-                            <Flex align="middle" justify="center" vertical>
-                                <Button type="primary" htmlType="submit" block>Sign in</Button>
-                                <span className="span-form">or</span>
-                                <Button type="link" onClick={() => navigate('/register')}>create an account!</Button>
-                            </Flex>
-                        </Form.Item>
-                    </Form>        
-                </Col>
-            </Row>
+                            <Form.Item>
+                                <Flex align="middle" justify="center" vertical>
+                                    <Button type="primary" htmlType="submit" block>Sign in</Button>
+                                    <span className="span-form">or</span>
+                                    <Button type="link" onClick={() => navigate('/register')}>create an account!</Button>
+                                </Flex>
+                            </Form.Item>
+                        </Form>        
+                    </Col>
+                </Row>
+            </div>
         </>
     )
 }
